a9a342f55a475fd16ea5dc750ab2966a87372b0c
lukman-nov <novawiyadi@gmail.com>
2025-02-09 01:47:37 +0700

    feat:(utils/misc) tailwind merge

--- web/src/utils/misc.ts
+++ web/src/utils/misc.ts
@@ -1,13 +1,15 @@
-// Will return whether the current environment is in a regular browser
-
 import { assetspath } from '../store/assetsPath';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-// and not CEF
 export const isEnvBrowser = (): boolean => !(window as any).invokeNative;
 
-// Basic no operation function
 export const noop = () => {};
 
 export const getItemUrl = (name: string) => {
   return `${assetspath}/${name}.png`;
 };
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
