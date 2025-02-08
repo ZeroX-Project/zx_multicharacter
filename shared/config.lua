3f089c309af3e25588962e4174e00c6de0c8808e
lukman-nov <novawiyadi@gmail.com>
2025-02-09 04:07:30 +0700

    chore:(shared/config) remove useless code

--- shared/config.lua
+++ shared/config.lua
@@ -1,26 +1,19 @@
 return {
   locale = 'en',
   setPedVisible = true,
-  defaultSpawn = vec4(-540.58, -212.02, 37.65, 208.88),
-  maxCharacter = 4,
+  defaultSpawn = vec4(-269.1097, -955.7875, 31.2231, 208.5021),
+  maxCharacter = 1,
   loadingModelsTimeout = 30000, -- Waiting time for ox_lib to load the models before throws an error, for low specs pc
 
   startingApartment = false,
-  enableDeleteButton = true, -- Whether players should be able to delete characters themselves.
+  enableDeleteButton = false, -- Whether players should be able to delete characters themselves.
 
-  dateFormat = 'dd/MM/yyyy', --- 'dd/MM/yyyy' | 'yyyy-MM-dd' | 'MM/dd/yyyy'
-  dateMin = '1900-01-01',    -- Has to be in the same format as the dateFormat config
-  dateMax = '2006-12-31',    -- Has to be in the same format as the dateFormat config
+  dateFormat = 'dd/MM/yyyy',  --- 'dd/MM/yyyy' | 'yyyy-MM-dd' | 'MM/dd/yyyy'
+  dateMin = '1900-01-01',     -- Has to be in the same format as the dateFormat config
+  dateMax = '2006-12-31',     -- Has to be in the same format as the dateFormat config
 
   profanityWords = {
     'bad word',
     'roleplay'
-  },
-
-  locations = { -- Spawn locations for multichar, these are chosen randomly
-    {
-      pedCoords = vector4(-1008.49, -474.51, 50.03, 208.64),
-      camCoords = vector4(-1005.53, -480.73, 50.52, 27.44),
-    },
   },
 }
