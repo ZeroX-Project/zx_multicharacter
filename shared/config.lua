47bcbc237c76f93fe198273b5b1ca938d2fc76ae
lukman-nov <novawiyadi@gmail.com>
2025-02-09 09:20:26 +0700

    feat:(shared/config.lua) description

--- shared/config.lua
+++ shared/config.lua
@@ -1,19 +1,15 @@
 return {
-  locale = 'en',
-  setPedVisible = true,
-  spawnAtLastLocation = true,
-  defaultSpawn = vec4(-269.1097, -955.7875, 31.2231, 208.5021),
-  maxCharacter = 1,
-  loadingModelsTimeout = 30000, -- Waiting time for ox_lib to load the models before throws an error, for low specs pc
-
-  startingApartment = false,
-  enableDeleteButton = false, -- Whether players should be able to delete characters themselves.
-
-  dateFormat = 'dd/MM/yyyy',  --- 'dd/MM/yyyy' | 'yyyy-MM-dd' | 'MM/dd/yyyy'
-  dateMin = '1900-01-01',     -- Has to be in the same format as the dateFormat config
-  dateMax = '2006-12-31',     -- Has to be in the same format as the dateFormat config
-
-  profanityWords = {
+  maxCharacter = 4,                                             -- Set maximum character in identifier/lisence
+  setPedVisible = true,                                         -- Set visible ped when in menu
+  spawnAtLastLocation = true,                                   -- If set to true, skips choice spawn points (requires qbx_spawn if true)
+  startingApartment = false,                                    -- If set to false, skips apartment choice in the beginning (requires qbx_spawn if true)
+  enableDeleteButton = true,                                    -- Whether players should be able to delete characters themselves.
+  loadingModelsTimeout = 30000,                                 -- Waiting time for ox_lib to load the models before throws an error, for low specs pc
+  defaultSpawn = vec4(-269.1097, -955.7875, 31.2231, 208.5021), -- Default spawn position
+  dateFormat = 'dd/MM/yyyy',                                    -- 'dd/MM/yyyy' | 'yyyy-MM-dd' | 'MM/dd/yyyy'
+  dateMin = '1900-01-01',                                       -- Use format yyyy-mm-dd
+  dateMax = '2010-12-31',                                       -- Use format yyyy-mm-dd
+  profanityWords = {                                            -- Blacklisted words for name creation
     'bad word',
     'roleplay'
   },
